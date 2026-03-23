34e1cb2f066a6e2068fd1db68861c932543947f7
sarandeepsingh <sandysaluja7@gmail.com>
2026-03-23 18:45:14 +0530

    Moved Kafka Consumer to execution service

---
+++ execution-service/assessment-execution-engine/src/test/java/com/example/assessment_execution_engine/AssessmentExecutionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assessment_execution_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentExecutionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
